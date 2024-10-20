c7c61a4342b4fd01b108fa7762f204a11360c4a2
Nacho Fayos Gómez <151563678+NachoFayos@users.noreply.github.com>
2024-10-20 06:52:54 +0200

    Add files via upload

---
+++ Desarrollo de interfaces/Entrega 1/C-Funciones del editor para ubicar los componentes del interfaz/Ejemplo de grid.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+
+ventana = tk.Tk()
+marco = tk.Frame(ventana)
+marco.grid(row=0, column=0)
+
+Item1 = tk.Entry(marco)
+Item1.grid(row=0, column=1)
+
+Item2 = tk.Label(marco, text="¡Hola! ¿Qué tal?")
+Item2.grid(row=1, column=2)
+
+button = tk.Button(marco, text="Púlsame")
+button.grid(row=2, column=3, columnspan=2)
+
+ventana.mainloop()
+s
